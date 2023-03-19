dffb6494479b4e65f68f30789cc5e561fe83f1cc
jsprofessional <jshuman@protonmail.com>
2023-03-18 21:32:04 -0500

    Added purple button styling.

--- src/components/width-container/WidthContainer.tsx
+++ src/components/width-container/WidthContainer.tsx
@@ -1,6 +1,6 @@
 export const WidthContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border-4 border-red-500 flex flex-col justify-between h-screen py-2 space-y-2 mx-auto lg:px-8 lg:mx-96">
+    <div className="flex flex-col justify-around h-screen py-2 space-y-2 mx-auto lg:px-8 lg:mx-96">
       {children}
     </div>
   );
