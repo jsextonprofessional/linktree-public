9c8bdfe427d20204ddae645163e927bdea20bc7d
jsprofessional <jshuman@protonmail.com>
2023-03-19 11:58:31 -0500

    Added rainbow bg color on hover.

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,46 @@ import "./App.css";
 function App() {
   const links = [
     {
-      url: "https://www.google.com",
+      url: "https://jacobsexton.pages.dev/",
       title: "Portfolio",
+      bgColor: "purple",
     },
     {
       url: "https://www.github.com/jsextonprofessional",
       title: "Github",
+      bgColor: "blue",
     },
     {
       url: "https://www.linkedin.com/in/jsextonprofessional/",
       title: "LinkedIn",
+      bgColor: "green",
     },
     {
       url: "https://www.instagram.com/jacob._.sexton/",
       title: "Instagram",
+      bgColor: "yellow",
     },
     {
       url: "#",
       title: "Email Me",
+      bgColor: "orange",
     },
     {
       url: "#",
       title: "Resume",
+      bgColor: "red",
     },
   ];
 
   return (
-    <div className="bg-purple-500">
+    <div className="bg-black">
       <WidthContainer>
         {links.map((link) => (
           <a
             href={link.url}
             target="_blank"
-            className="border-8 border-black py-4 text-center text-black text-2xl font-black shadow-[0_1rem_1rem_1rem_rgba(255, 255, 255)] hover:text-white hover:bg-black hover:border-white"
+            className={`bg-black border-8 border-white py-8 text-center text-white text-2xl font-black
+              ${link.bgColor}`}
           >
             {link.title}
           </a>
