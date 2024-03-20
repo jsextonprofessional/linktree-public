387fe97ec1110fbc809ccedff1c5b9863acb0aae
jsprofessional <jshuman@protonmail.com>
2024-03-20 13:29:01 -0500

    Removes appended URL.

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
     },
     {
       key: 5,
-      url: "https://drive.google.com/file/d/1KWZSUeuGeYfzJqlBEq1MA-XWcDDYyJFSLKN4-_Z77yg/view?pli=1",
+      url: "https://drive.google.com/file/d/1KWZSUeuGeYfzJqlBEq1MA-XWcDDYyJFSLKN4-_Z77yg/view",
       title: "resume",
       bgColor: "red",
     },
