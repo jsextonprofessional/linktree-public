01148c588c46ab25fc43b7fadd8a07db0a391ed5
Jacob Sexton <jsextonprofessional@protonmail.com>
2024-08-01 16:46:18 -0400

    Moved links array into constants. Updated resume link with evergreen public link.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
 import { WidthContainer } from "../src/components/width-container/WidthContainer";
 import "./App.css";
+import { links } from "./constants";
 
 function App() {
-  const links = [
-    {
-      key: 0,
-      url: "https://jacobsexton.cool/",
-      title: "portfolio",
-      bgColor: "purple",
-    },
-    {
-      key: 1,
-      url: "https://www.github.com/jsextonprofessional",
-      title: "github",
-      bgColor: "blue",
-    },
-    {
-      key: 2,
-      url: "https://www.linkedin.com/in/jsextonprofessional/",
-      title: "linkedin",
-      bgColor: "green",
-    },
-    {
-      key: 3,
-      url: "https://www.instagram.com/jacob._.sexton/",
-      title: "instagram",
-      bgColor: "yellow",
-    },
-    {
-      key: 4,
-      url: "#",
-      title: "email me",
-      bgColor: "orange",
-    },
-    {
-      key: 5,
-      url: "https://drive.google.com/file/d/1KWZSUeuGeYfzJqlBEq1MA-XWcDDYyJFSLKN4-_Z77yg/view",
-      title: "resume",
-      bgColor: "red",
-    },
-  ];
 
   const emailMe = () => {
     window.open("mailto:jsextonprofessional@protonmail.com");
